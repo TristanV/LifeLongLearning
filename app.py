f9e84cf0110547a2c1f4e571e22578a82844a201
Tristan Vanrullen <tristan.vanrullen@gmail.com>
2025-05-30 01:37:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,8 +77,8 @@ with tabs[0]:
     st.sidebar.header("Paramètres")
 
     with st.sidebar.expander("Niveau de référence", expanded=False):
-        R0 = st.slider("R0 (Niveau de référence initial)", 100, 5000, 1000)
-        k = st.slider("k (Taux de croissance exponentielle du niveau de référence)", 0.01, 0.1, 0.03)
+        R0 = st.slider("R0 (Niveau de référence initial)", 100, 5000, 4000, step=100)
+        k = st.slider("k (Taux de croissance exponentielle du niveau de référence)", 0.001, 0.1, 0.005)
 
     with st.sidebar.expander("Courbe d'auto-évaluation", expanded=False):
         pente_sigmoide = st.slider("pente_sigmoide (raideur sigmoïde)", 0.01, 5.0, 1.0, step=0.01)
